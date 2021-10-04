2a0f57838ec9359e2c1a94b05f04b5a9b978cc84
Feyza Topgul <feyzatopgul@gmail.com>
2021-10-03 18:24:03 -0700

    Add User Defaults code

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    
+    @IBAction func submitButton(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.setValue(nameField.text, forKey: "name")
+        defaults.setValue(emailField.text, forKey: "email")
+        defaults.setValue(phoneField.text, forKey: "phone")
+        if let name = defaults.string(forKey: "name"){
+            nameLabel.text = name
+        }
+        if let email = defaults.string(forKey: "email"){
+            emailLabel.text = email
+        }
+        if let phone = defaults.string(forKey: "phone"){
+            phoneLabel.text = phone
+        }
+    }
+  
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let defaults = UserDefaults.standard
+        if let name = defaults.string(forKey: "name"){
+            nameLabel.text = name
+        }
+        if let email = defaults.string(forKey: "email"){
+            emailLabel.text = email
+        }
+        if let phone = defaults.string(forKey: "phone"){
+            phoneLabel.text = phone
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
